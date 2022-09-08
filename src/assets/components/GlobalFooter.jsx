737e8303b9e1655e46ef8dfbc2e61d29cf7947f0
0xENDER <72353925+0xENDER@users.noreply.github.com>
2022-09-08 19:02:45 +0300

    Fix a typo

--- src/assets/components/GlobalFooter.jsx
+++ src/assets/components/GlobalFooter.jsx
@@ -6,7 +6,7 @@
 import styles from './../styles/globalFooter.module.css';
 function GlobalFooter(props){
     return (
-        <div class={styles.globalbar} data-show-content={props.showContent}>Terms and Conditions | About This Service | About 0x0C</div>
+        <div class={styles.globalfooter} data-show-content={props.showContent}>Terms and Conditions | About This Service | About 0x0C</div>
     );
 }
 
