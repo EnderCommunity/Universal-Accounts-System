15bb8dab63bac6d6d665d76475efd5cc8e30bfb6
0xENDER <72353925+0xENDER@users.noreply.github.com>
2022-09-24 23:50:39 +0300

    Add PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,40 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-    plugins: [solidPlugin()],
+    plugins: [
+        solidPlugin(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true
+            },
+            manifest: {
+                name: 'MyPWA',
+                short_name: 'MyPWA',
+                description: 'My PWA for you!',
+                theme_color: '#ffffff',
+                icons: [{
+                        src: '/images/icon-192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/images/icon-512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/images/icon-512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable'
+                    }
+                ]
+            }
+        })
+    ],
     server: {
         port: 4000,
     },
