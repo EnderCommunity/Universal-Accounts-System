62858b529aab60e468af56daa463f5bb46ad483c
0xENDER <72353925+0xENDER@users.noreply.github.com>
2022-10-07 11:37:50 +0300

    Fix a bug

--- dist/assets/CustomElements.16cf6f96.js
+++
@@ -1,1 +0,0 @@
-import{a as r,b as t,t as a}from"./index.7527d467.js";import{g as o}from"./FlexContainer.b17aa88c.js";const s=a("<hr>");function i(){return(()=>{const e=s.cloneNode(!0);return r(()=>t(e,o.divider)),e})()}export{i as D};
